7d380bb21230875e3293510df45934f3d5811bcb
Oliver Lock <oliverlock@ntlworld.com>
2022-08-16 17:59:45 +0100

    Begun Working on question 1941

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -4,58 +4,10 @@
 
 
 
-Console.WriteLine(LongestCommonPrefix.Answer());
+Console.WriteLine(CheckIfAllCharactersHaveEqualNumberofOccurrences.Answer());
 
 
 
 
 
-
-
-
-
-public static class LeetCodea
-{
-    public static string Current()
-    {
-        string[] strs = { "ab", "a" };
-        var output = "";
-        var charCount = 0;
-        var outputTemp = "";
-        List<char[]> charsList = new List<char[]>();
-
-        if (strs.Count() == 0)
-            return "";
-        if (strs.Count() == 1)
-            return strs[0];
-
-
-        for (int i = 0; i <= strs.Count() - 1; i++)
-        {
-            var charArray = strs[i].ToCharArray();
-            charsList.Add(charArray);
-        }
-        while (charCount < charsList[0].Count())
-        {
-
-            outputTemp += charsList[0][charCount];
-            foreach (var item in charsList)
-            {
-                if (charCount > item.Count() - 1)
-                {
-                    return output;
-                }
-                if (outputTemp[charCount] != item[charCount])
-                {
-                    return output;
-                }
-            }
-            output += charsList[0][charCount];
-            charCount++;
-        }
-        
-        return output;
-    }
-
-}
 
