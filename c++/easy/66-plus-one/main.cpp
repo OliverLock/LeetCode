22e5a096fe4d813d7ba885b3d773797f97bfdc83
Oliver Lock <oliverlock@ntlworld.com>
2024-12-28 16:47:50 +0000

    Unoptomised version of question 66

--- c++/easy/66-plus-one/main.cpp
+++ c++/easy/66-plus-one/main.cpp
@@ -1,33 +1,59 @@
 #include <iostream>
 #include <ostream>
 #include <vector>
-class Soloution {
+class Solution {
 public:
-  std::vector<int> plusOne(std::vector<int> &digits) { 
-    std:: vector<int> newVector = {};
-    int carryOver = false;
-    for (int i = digits.size() - 1; i >= 0; i--){
-      
-
+  std::vector<int> plusOne(std::vector<int> &digits) {
+    bool carryOver = false;
+    for (int i = digits.size() - 1; i >= 0; i--) {
+      // Iterate backwards and check if it would go over 9 then carry over
+      if (carryOver == true) {
+        digits[i] += 1;
+        carryOver = false;
+      }
+      if (i == digits.size() - 1) {
+        digits[i] += 1;
+      }
+      if (digits[i] - 10 >= 0) {
+        carryOver = true;
+        digits[i] = digits[i] - 10;
+      }
     }
-    
-
-    return {0, 0, 0}; 
-
-
+    // For situation where final element in array needs carry over
+    if (carryOver == true) {
+      int numberHold = 0;
+      for (int i = 0; i < digits.size(); i++) {
+        if (i == 0) {
+          numberHold = digits[i];
+          digits[i] = 1;
+        }
+        if (i != 0) {
+          int temp = digits[i];
+          digits[i] = numberHold;
+          numberHold = temp;
+        }
+        if (i == digits.size() - 1) {
+          digits.push_back(0);
+          return digits;
+        }
+      }
+    }
+    return digits;
   }
 };
 
 int main() {
   std::cout << "Running Soloution" << std::endl;
-  Soloution soloution;
-  std::vector<int> inputVector = {1, 2, 3, 4};
+  Solution solution;
+  std::vector<int> inputVector = {0,1,2,3,4,5,6};
+  std::string test = "test";
 
-  std::vector<int> data = soloution.plusOne(inputVector);
+  std::vector<int> data = solution.plusOne(inputVector);
 
   for (int num : data) {
     std::cout << num << "";
   }
+  std::cout << std::endl;
   std::cout << std::endl;
   std::cout << "test" << std::endl;
   return 0;
